ab54f688d2b915b3f354226dca528d3e9b560013
Dirk Grappendorf <dirk@grappendorf.net>
2014-09-25 18:38:12 +0200

    Bump version to 1.0.0-r4

--- package-all.sh
+++ package-all.sh
@@ -2,6 +2,6 @@
 DIR=arduino-eclipse
 mkdir -p package-build
 cd ..
-rm -f $DIR/package-build/arduino-eclipse-1.0-r3.zip
-zip -r $DIR/package-build/arduino-eclipse-1.0-r3.zip $DIR -x $DIR/\*.git\* -x $DIR/\*_\* -x $DIR/target
+rm -f $DIR/package-build/arduino-eclipse-1.0.0-r4.zip
+zip -r $DIR/package-build/arduino-eclipse-1.0.0-r4.zip $DIR -x $DIR/\*.git\* -x $DIR/\*_\* -x $DIR/target
 cd $DIR
