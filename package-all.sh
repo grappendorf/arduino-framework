bc0ab55fa8e8e94d7fa1a1024e0966d0cc940995
Dirk Grappendorf <dirk@grappendorf.net>
2015-03-01 19:31:31 +0100

    Bump version

--- package-all.sh
+++ package-all.sh
@@ -2,6 +2,6 @@
 DIR=arduino-framework
 mkdir -p package-build
 cd ..
-rm -f $DIR/package-build/arduino-framework-1.0.0-r6.zip
-zip -r $DIR/package-build/arduino-framework-1.0.0-r6.zip $DIR -x $DIR/\*.git\* -x $DIR/\*_\* -x $DIR/target
+rm -f $DIR/package-build/arduino-framework-1.0.0-r7.zip
+zip -r $DIR/package-build/arduino-framework-1.0.0-r7.zip $DIR -x $DIR/\*.git\* -x $DIR/\*_\* -x $DIR/target
 cd $DIR
